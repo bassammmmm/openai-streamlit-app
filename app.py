b1af90aecc4948bf6a79f8bde17e111bb6c9de0f
Bassam Emad <bassamemad000@gmail.com>
2023-12-12 02:58:04 +0200

    model changing

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from PyPDF2 import PdfReader
 import pandas as pd
 import openai
 import os
+import chardet
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
 # Initialize history session
 if 'chat_history' not in st.session_state:
@@ -19,21 +20,37 @@ def generate_answer(api_key, query):
                 "content": query,
             }
         ],
-        model="gpt-3.5-turbo",
+        model="gpt-4",
     )
     return response.choices[0].message.content
 
 # Process the uploaded file (PDF or CSV)
 def process_file(file, file_type):
-    if file_type == 'pdf':
+    if file_type == 'application/pdf':
         text = ""
         pdf_reader = PdfReader(file)
         for page in pdf_reader.pages:
             text += page.extract_text()
         return text
-    elif file_type == 'csv':
-        df = pd.read_csv(file)
-        return df.to_string(index=False)  # Convert dataframe to a string for simplicity
+    else:
+        print(file_type)
+        if file_type == 'text/csv':
+            print('im here')
+            df = pd.read_csv(file)
+        #elif file_type == 'application/vnd.ms-excel':
+        #    df = pd.read_excel(file, engine='xlrd')  # For .xls format
+        #elif file_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+        #    df = pd.read_excel(file, engine='xlrd')  # For .xlsx format
+        else:
+            return 'EMPTY CONTENT'
+                    
+        df_text = df.to_string(index=False)
+        return df_text
+
+
+def create_vertical_space(times):
+    for _ in range(times):
+        st.markdown("")
 
 def main():
     # OpenAI API key
@@ -45,9 +62,9 @@ def main():
 
     if file:
         try:
-            file_type = 'pdf' if file.type == 'application/pdf' else 'csv'
+            file_type = str(file.type)
             file_text = process_file(file, file_type)
-
+            print(file_text)
             # Making a query using file content
             query = st.text_input("Ask questions about the file.")
             if query:
@@ -57,24 +74,26 @@ def main():
                     response = generate_answer(api_key, query=new_query)
                     
                     # Store user query and AI response in session state chat history
-                    st.session_state['chat_history'].append({"role": "(Replying on)", "content": query})
-                    st.session_state['chat_history'].append({"role": "", "content": response})
-                    
+                    content = {
+                        'reply_on': query,
+                        'message' : response
+                    }
+                    st.session_state['chat_history'].append(content)
+                                    
                     # Display chat history with auto-scroll to the latest message
-                    latest_messages = st.session_state['chat_history'][-2:]  # Get the last two messages
+                    latest_messages = st.session_state['chat_history'][-1:]  # Get the last two messages
                     for chat in latest_messages:
-                        if not chat['role'] == "":
-                            st.write(f"{chat['role']}: {chat['content']}")
-                        else:
-                            st.write(f"{chat['content']}")
-                    for chat in st.session_state['chat_history'][:-2]:
-                        if not chat['role'] == "":
-                            st.write(f"{chat['role']}: {chat['content']}")
-                        else:
-                            st.write(f"{chat['content']}")
-            st.write("\n\n\n")
+                        st.markdown(f"***(Replying On)***: ***{chat['reply_on']}***")
+                        st.markdown(f"{chat['message']}")
+                        create_vertical_space(3)
+                        
+                    for chat in reversed(st.session_state['chat_history'][:-1]):
+                        st.markdown(f"***(Replying On)***: ***{chat['reply_on']}***")
+                        st.markdown(f"{chat['message']}")
+                        create_vertical_space(3)
         except:
-            st.write('Something went wrong.')
+            st.markdown(f"***Something went wrong.***")
+        
 
 if __name__ == '__main__':
     main()
