fc92f56258bb2d50a87dd1406d1c6918cdab4c09
Bassam Emad <bassamemad000@gmail.com>
2023-12-14 00:23:59 +0200

    langchain

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
-from utils import *
+from utils import *  # Importing functions from a 'utils' module
 
 # Initialize history session
-if 'chat_history' not in st.session_state:  # Check if 'chat_history' is not in the Streamlit session state
-    st.session_state['chat_history'] = []  # Initialize 'chat_history' as an empty list if not present
+if 'chat_history' not in st.session_state:  # Check if 'chat_history' is not in the Streamlit session
+    st.session_state['chat_history'] = []  # Initialize 'chat_history' as an empty list if not there
 
 def main():
     # Allow users to upload a file (supports PDF or CSV)
     file = st.file_uploader("Upload Your File", type=['pdf', 'csv'])
     
-    if file:  # If a file is uploaded
+    if file is not None:  # If a file is uploaded
         
         file_type = str(file.type)  # Get the type of the uploaded file
         query = st.text_input("Ask questions about the file.")  # Input field for user queries
+        cancel_button = st.button('Cancel')
+        knowledgeBase = process_text(process_file(file, file_type))  # Process uploaded file and create a knowledge base
+        
+        if cancel_button:  # If 'Cancel' button is clicked, stop
+            st.stop()
         
         if query:  # If a query is entered by the user
-
-            response = process_file(file, file_type, query)  # Process the uploaded file based on its type and the query entered
-
+            # Search for similar documents based on the query
+            docs = knowledgeBase.similarity_search(query)
+            
+            # Initialize a ChatOpenAI instance and load a question answering chain
+            llm = ChatOpenAI(openai_api_key=api_key)
+            chain = load_qa_chain(llm, chain_type='stuff')
+            
+            # Execute the chain with the query
+            with get_openai_callback() as cost:
+                response = chain.run(input_documents=docs, question=f'(Your name is Structured Financials Assistant). PLEASE BE SO DETAILED. {query}')
+                print(cost)  # Print the cost of the OpenAI request
+                
+            # Store query and AI-generated response in a dictionary
             content = {
-                'reply_on': query,  # Store the query to which the AI responds
-                'message' : response  # Store the AI-generated response
+                'reply_on': query,
+                'message' : response
             }
             st.session_state['chat_history'].append(content)  # Append the AI response to the chat history
 
-            latest_message = st.session_state['chat_history'][-1:]  # Get the latest message from the chat history
+            # Display the latest message from the chat history
+            latest_message = st.session_state['chat_history'][-1:]
             st.markdown(f"***(Replying On)***: ***{latest_message[0]['reply_on']}***")  # Display the query the AI is responding to
             st.markdown(f"{latest_message[0]['message']}")  # Display the AI-generated response
             create_vertical_space(3)  # Create vertical space between chat messages
                 
-            for chat in reversed(st.session_state['chat_history'][:-1]):  # Iterate over the rest of the chat history in reverse order
+            # Display the rest of the chat history in reverse order
+            for chat in reversed(st.session_state['chat_history'][:-1]):
                 st.markdown(f"***(Replying On)***: ***{chat['reply_on']}***")  # Display the query the AI responded to
                 st.markdown(f"{chat['message']}")  # Display the AI-generated response
                 create_vertical_space(3)  # Create vertical space between chat messages
--- utils.py
+++ utils.py
@@ -1,86 +1,55 @@
 import streamlit as st
-import pandas as pd
-import openai
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import pandas as pd
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 
-# Generate an answer using ChatGPT
-def generate_answer(api_key, query):
-    """Generates an answer using the OpenAI GPT-3 API."""
-    client = openai.OpenAI(api_key=api_key)  # Initialize OpenAI client
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": query,  # Query to be sent
-            }
-        ],
-        model="gpt-3.5-turbo",  # Use GPT-3.5-turbo model
-    )
-    return response.choices[0].message.content  # Return the generated response
+# Importing Langchain modules
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from langchain.chat_models import ChatOpenAI
 
-def process_page(page, api_key, query):
-    """Gets a response for an individual page of a PDF."""
-    text = f'As Structured Financials AI-Powered Assistant. Based on:{page.extract_text()}. {query}'  # Extracted text from the page
-    return generate_answer(api_key, query=text) + "\n"  # Return generated response for the page
+load_dotenv()
+api_key = os.environ.get('API_KEY')
 
-def process_chunk(chunk, api_key, query):
-    """Gets a response for an chunk of an Excel file."""
-    chunk_text = f'As Structured Financials AI-Powered Assistant. Based on:{chunk}.{query}'  # Chunk of data from CSV
-    return generate_answer(api_key, query=chunk_text) + "\n"  # Return generated response for the chunk
-
-def process_file(file, file_type, query):
-    """Processes the uploaded file based on its type (PDF or CSV)."""
-    load_dotenv()  # Load environment variables
-    api_key = os.environ.get('OPENAI_API_KEY')  # Get OpenAI API key from environment
-    response = ""  # Initialize response variable
-
+# Function to process different types of files
+def process_file(file, file_type):
     if file_type == 'application/pdf':  # For PDF files
         pdf_reader = PdfReader(file)  # Read PDF file
-        pages_length = len(pdf_reader.pages)  # Get number of pages
-        time_per_page = 3.46  # Estimated time per page processing
-
-        eta_seconds = pages_length * time_per_page  # Calculate total processing time
-        eta_minutes, eta_seconds = divmod(eta_seconds, 60)  # Convert processing time to minutes and seconds
-        
-        # Create an ETA message based on the number of pages
-        message = f'Digging deep into your {pages_length} page(s)... \
-            ETA {int(eta_minutes)} minute(s) and {int(eta_seconds)} second(s).' \
-                if not int(eta_minutes) == 0 else \
-                    f'Digging deep into your {pages_length} page(s)... \
-                    ETA {int(eta_seconds)} second(s).'
-                    
-        with st.spinner(message):  # Display ETA in Streamlit spinner
-            with ThreadPoolExecutor(max_workers=5) as executor:  # Use ThreadPoolExecutor for concurrent processing
-                # Process each page concurrently
-                future_to_page = {
-                    executor.submit(process_page, page, api_key, query): page
-                    for page in pdf_reader.pages
-                }
-
-                for future in as_completed(future_to_page):  # As each future is completed
-                    response += future.result() + "\n"  # Add the result to the response
-                
-        return response  # Return the accumulated response
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text  # Return the file text
     
     elif file_type == 'text/csv':  # For CSV files
-        chunksize = 30000  # Define chunk size for reading CSV
-        df_chunks = pd.read_csv(file, encoding='utf-8', chunksize=chunksize)  # Read CSV in chunks
+        df = pd.read_csv(file, encoding='utf-8')  # Read CSV 
+        return df.to_string(index=False)  # Return the file text
 
-        with st.spinner('Digging deep into your rows... ') as spin:  # Display message in Streamlit spinner
-            with ThreadPoolExecutor(max_workers=5) as executor:  # Use ThreadPoolExecutor for concurrent processing
-                # Process each chunk of the CSV concurrently
-                futures = {executor.submit(process_chunk, chunk, api_key, query): chunk for chunk in df_chunks}
-                for future in as_completed(futures):  # As each future is completed
-                    response += future.result()  # Add the result to the response
-
-        return response  # Return the accumulated response
-
-    else:  # If file type is not supported
+    else: 
         return st.write('Something went wrong.')  # Display an error message
 
+# Function to process text and generate a knowledge base
+def process_text(text):
+    # Split the text into chunks using Langchain's CharacterTextSplitter
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    
+    # Convert the chunks of text into embeddings to form a knowledge base
+    embeddings = OpenAIEmbeddings()
+    knowledgeBase = FAISS.from_texts(chunks, embeddings)
+    
+    return knowledgeBase
+
+# Function to create vertical space in Streamlit app
 def create_vertical_space(times):
     """Creates vertical space in the Streamlit app."""
     for _ in range(times):  # Loop for specified number of times
