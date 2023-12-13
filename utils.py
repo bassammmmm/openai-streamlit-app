a266064e2a403018400a5d310db9e50229349a56
Bassam Emad <bassamemad000@gmail.com>
2023-12-14 00:39:21 +0200

    remove useless import

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from langchain.chat_models import ChatOpenAI
 
